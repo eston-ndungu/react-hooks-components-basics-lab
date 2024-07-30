81a5c44ad0ebada2f7869bb4348f3c398bad2281
ESTON NDUNGU <estonndungu1@gmail.com>
2024-07-30 11:44:07 +0300

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -31,9 +31,7 @@ function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
       <Home/>
-      {/* add your <About> component here */}
       <About/>
     </div>
   );
